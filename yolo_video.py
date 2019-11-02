bd61d037c29b456ed724f4d019fbd23fb3bfda51
Aakash Praliya <aprali18@in.ibm.com>
2019-11-03 01:53:59 +0530

    update

--- yolo_video.py
+++ yolo_video.py
@@ -9,6 +9,14 @@ import time
 import cv2
 import os
 
+def is_valid_IOU(bbox1, bbox2, threshold):
+	minx = min(bbox1[0]+bbox1[2],bbox2[0]+bbox2[2])
+	miny = min(bbox1[1]+bbox1[3],bbox2[1]+bbox2[3])
+	maxx = max(bbox1[0],bbox2[0])
+	maxy = max(bbox1[1],bbox2[1])
+	intersection = (maxx-minx)*(maxy-miny)
+	return intersection/(bbox1[2]*bbox1[3] + bbox2[2]*bbox2[3] - intersection) > threshold
+
 (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
@@ -89,6 +97,7 @@ else:
 
 
 frame_number = 0
+ok = None
 # loop over frames from the video file stream
 while True:
 	frame_number += 1
@@ -104,105 +113,130 @@ while True:
 	if W is None or H is None:
 		(H, W) = frame.shape[:2]
 
-	# construct a blob from the input frame and then perform a forward
-	# pass of the YOLO object detector, giving us our bounding boxes
-	# and associated probabilities
-	blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416),
-		swapRB=True, crop=False)
-	net.setInput(blob)
-	start = time.time()
-	layerOutputs = net.forward(ln)
-	end = time.time()
+	if frame_number%5 == 0 or ok is None:
+		# construct a blob from the input frame and then perform a forward
+		# pass of the YOLO object detector, giving us our bounding boxes
+		# and associated probabilities
+		blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416),
+			swapRB=True, crop=False)
+		net.setInput(blob)
+		start = time.time()
+		layerOutputs = net.forward(ln)
+		end = time.time()
 
-	# initialize our lists of detected bounding boxes, confidences,
-	# and class IDs, respectively
-	boxes = []
-	confidences = []
-	classIDs = []
+		# initialize our lists of detected bounding boxes, confidences,
+		# and class IDs, respectively
+		boxes = []
+		confidences = []
+		classIDs = []
 
-	# loop over each of the layer outputs
-	for output in layerOutputs:
-		# loop over each of the detections
-		for detection in output:
-			# extract the class ID and confidence (i.e., probability)
-			# of the current object detection
-			scores = detection[5:]
-			classID = np.argmax(scores)
-			confidence = scores[classID]
+		# loop over each of the layer outputs
+		for output in layerOutputs:
+			# loop over each of the detections
+			for detection in output:
+				# extract the class ID and confidence (i.e., probability)
+				# of the current object detection
+				scores = detection[5:]
+				classID = np.argmax(scores)
+				confidence = scores[classID]
 
-			# filter out weak predictions by ensuring the detected
-			# probability is greater than the minimum probability
-			if confidence > args["confidence"]:
-				# scale the bounding box coordinates back relative to
-				# the size of the image, keeping in mind that YOLO
-				# actually returns the center (x, y)-coordinates of
-				# the bounding box followed by the boxes' width and
-				# height
-				box = detection[0:4] * np.array([W, H, W, H])
-				(centerX, centerY, width, height) = box.astype("int")
+				# filter out weak predictions by ensuring the detected
+				# probability is greater than the minimum probability
+				if confidence > args["confidence"]:
+					# scale the bounding box coordinates back relative to
+					# the size of the image, keeping in mind that YOLO
+					# actually returns the center (x, y)-coordinates of
+					# the bounding box followed by the boxes' width and
+					# height
+					box = detection[0:4] * np.array([W, H, W, H])
+					(centerX, centerY, width, height) = box.astype("int")
 
-				# use the center (x, y)-coordinates to derive the top
-				# and and left corner of the bounding box
-				x = int(centerX - (width / 2))
-				y = int(centerY - (height / 2))
+					# use the center (x, y)-coordinates to derive the top
+					# and and left corner of the bounding box
+					x = int(centerX - (width / 2))
+					y = int(centerY - (height / 2))
 
-				# update our list of bounding box coordinates,
-				# confidences, and class IDs
-				boxes.append([x, y, int(width), int(height)])
-				confidences.append(float(confidence))
-				classIDs.append(classID)
+					# update our list of bounding box coordinates,
+					# confidences, and class IDs
+					boxes.append([x, y, int(width), int(height)])
+					confidences.append(float(confidence))
+					classIDs.append(classID)
 
-	# apply non-maxima suppression to suppress weak, overlapping
-	# bounding boxes
-	idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"],
-		args["threshold"])
+		# apply non-maxima suppression to suppress weak, overlapping
+		# bounding boxes
+		idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"],
+			args["threshold"])
 
-	# ensure at least one detection exists
-	if len(idxs) > 0:
-		# loop over the indexes we are keeping
-		for i in idxs.flatten():
-			# extract the bounding box coordinates
-			(x, y) = (boxes[i][0], boxes[i][1])
-			(w, h) = (boxes[i][2], boxes[i][3])
+		# ensure at least one detection exists
+		if len(idxs) > 0:
+			# loop over the indexes we are keeping
+			for i in idxs.flatten():
+				# extract the bounding box coordinates
+				(x, y) = (boxes[i][0], boxes[i][1])
+				(w, h) = (boxes[i][2], boxes[i][3])
 
-			x = max(0,x)
-			y = max(0,y)
+				x = max(0,x)
+				y = max(0,y)
+				text = "{}: {:.4f}".format(LABELS[classIDs[i]],
+					confidences[i])
+				if 'face' in text.lower():
+					# draw a bounding box rectangle and label on the frame
+					sub_face = frame[y:y+h, x:x+w]
+					# apply a gaussian blur on this new recangle image
+					sub_face = cv2.GaussianBlur(sub_face,(23, 23), 30)
+					# merge this blurry rectangle to our final image
+					frame[y:y+sub_face.shape[0], x:x+sub_face.shape[1]] = sub_face
 
-			if frame_number == 1:
-				xt = x - 0.3*w
-				yt = y - 0.3*h
-				wt = 1.6*w
-				ht = 1.6*h
+					color = [int(c) for c in COLORS[classIDs[i]]]
+					cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+					cv2.putText(frame, text, (x, y - 5),
+						cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+					xt = x - 0.3*w
+					yt = y - 0.3*h
+					wt = 1.6*w
+					ht = 1.6*h
+					if frame_number == 0:
+						tracker.init(frame, (xt,yt,wt,ht))
+						ok = None
+					else:
+						ok, bbox = tracker.update(frame)
+						if not is_valid_IOU(boxes[i],bbox,0.5):
+							print("IOU Failed")
+							tracker.init(frame, (xt,yt,wt,ht))
+							ok = None
+				else:
+					ok, bbox = tracker.update(frame)
+				if ok:
+					# Tracking success
+					p1 = (int(bbox[0]), int(bbox[1]))
+					p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+					sub_face = frame[p1[1]:p2[1], p1[0]:p2[0]]
+					# apply a gaussian blur on this new recangle image
+					sub_face = cv2.GaussianBlur(sub_face,(23, 23), 30)
+					# merge this blurry rectangle to our final image
+					frame[p1[1]:p1[1]+sub_face.shape[0], p1[0]:p1[0]+sub_face.shape[1]] = sub_face
+					cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
+				else :
+					# Tracking failure
+					cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
+	else:
+		if ok:
+			ok, bbox = tracker.update(frame)
+			# Tracking success
+			p1 = (int(bbox[0]), int(bbox[1]))
+			p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+			sub_face = frame[p1[1]:p2[1], p1[0]:p2[0]]
+			# apply a gaussian blur on this new recangle image
+			sub_face = cv2.GaussianBlur(sub_face,(23, 23), 30)
+			# merge this blurry rectangle to our final image
+			frame[p1[1]:p1[1]+sub_face.shape[0], p1[0]:p1[0]+sub_face.shape[1]] = sub_face
+			cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
+		else:
+			# Tracking failure
+			cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
 
-				tracker.init(frame, (xt,yt,wt,ht))
-				ok = None
-			else:
-				ok, bbox = tracker.update(frame)
 
-			text = "{}: {:.4f}".format(LABELS[classIDs[i]],
-				confidences[i])
-			if 'face' in text.lower():
-				# draw a bounding box rectangle and label on the frame
-				sub_face = frame[y:y+h, x:x+w]
-				# apply a gaussian blur on this new recangle image
-				sub_face = cv2.GaussianBlur(sub_face,(23, 23), 30)
-				# merge this blurry rectangle to our final image
-				frame[y:y+sub_face.shape[0], x:x+sub_face.shape[1]] = sub_face
-
-			color = [int(c) for c in COLORS[classIDs[i]]]
-			cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
-			cv2.putText(frame, text, (x, y - 5),
-				cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-
-			if ok:
-				# Tracking success
-				p1 = (int(bbox[0]), int(bbox[1]))
-				p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-				cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
-			else :
-				# Tracking failure
-				cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
-
+	print("Frame number:",frame_number)
 	# check if the video writer is None
 	if writer is None:
 		# initialize our video writer
